fd3169bbae6a32d9a078a92db145644e7e869170
Destrage <evanvictorpaul4@gmail.com>
2023-05-14 16:23:36 +0530

    optimal solution

--- trapping-rain-water/optimisedSolution.js
+++ trapping-rain-water/optimisedSolution.js
@@ -1,50 +1,53 @@
-// solution using 2 ponter solution
-
 // check the edge cases
 //  1. numberArray should contain atleast 2 element
 //  2. numberArray should be an array of possitive numbers
 //  3. target should be possitive number
 
-function containerWithMostWater(height) {
+// TC = O(N^2)
+// SC = O(1)
+
+function trappingRainWater(height) {
     try {
         checkEdgeCases(height);
 
-        if (height.length <= 1) return 0;
-        let maxArea = 0;
-        let p1 = 0;
-        let p2 = height.length - 1;
+        let totalWater = 0;
+        let lp = 0;
+        let rp = height.length - 1;
+        let maxl = 0;
+        let maxr = 0;
 
-        while (p1 < p2) {
-            const area = Math.min(height[p1], height[p2]) * (p2 - p1)
-            maxArea = Math.max(area, maxArea);
-            if (height[p1] > height[p2]) p2--
-            else p1++
+
+
+        while (lp !== rp) {
+            if (height[lp] <= height[rp]) {
+                maxl = Math.max(height[lp], maxl);
+                totalWater += maxl - height[lp] >= 0 ? maxl - height[lp] : 0;
+                lp++;
+            }
+            else if (height[lp] > height[rp]) {
+                maxr = Math.max(height[rp], maxr);
+                totalWater += maxr - height[rp] >= 0 ? maxr - height[rp] : 0;
+                rp--;
+            }
         }
-        return maxArea;
+
+        return totalWater;
 
     } catch (error) {
         return 0;
     }
 }
 
-function checkEdgeCases(numberArray) {
-    try {
-        if (!Array.isArray(numberArray)) throw new Error();
-        if (numberArray.length <= 1) throw new Error();
-        numberArray.forEach(num => {
-            if (num < 0) throw new Error();
-        });
-    } catch (error) {
-        return 0;
-    }
-
+function checkEdgeCases([...numberArray]) {
+    if (!Array.isArray(numberArray)) throw new Error();
+    if (numberArray.length <= 2) throw new Error();
+    if (numberArray.sort()[0] < 0) throw new Error();
 }
 
 
 require('./test-cases.json').forEach(([numberArray, expectedResult], index) => {
-    console.time('run');
-    result = containerWithMostWater(numberArray);
-    console.timeEnd('run');
+    result = trappingRainWater(numberArray);
+    console.log();
     if (expectedResult === 0 && result === 0) console.log(`test ${++index} passed`);
     else if (expectedResult == result) console.log(`test ${++index} passed`);
     else console.log(`test ${++index} failed`);
